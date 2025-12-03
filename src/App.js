8dc1395f7109ccb8cd385007e7d457d7dee90da2
Harmmerton Kogi <hammertonkogey@gmail.com>
2025-12-03 23:07:28 +0300

    Added a some routing for the menu-items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import { Homepage } from './pages/home/homepage.component';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function Hatspage() {
+  return <h1>Hats Page</h1>;
+}
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' Component={Homepage} />
+          <Route exact path='/hats' Component={Hatspage} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
