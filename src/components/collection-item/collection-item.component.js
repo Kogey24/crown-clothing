e4edbd149468c2bf0951cdefb4a7deb9f39e43a0
Harmmerton Kogi <hammertonkogey@gmail.com>
2025-12-06 22:44:46 +0300

    Resolve error to show the respective collection items

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -2,22 +2,29 @@ import React from "react";
 import './collection-item.styles.scss';
 
 
-const CollectionItems = (id, name, imageUrl, price) => (
-    <div className="collection-item" key={id}>
-        <div
-            className="image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-        />
+const CollectionItems = ({ id, name, imageUrl, price }) => {
+    console.log(imageUrl);
+    console.log(id);
+    console.log(name);
+    console.log(price);
 
-        <span className="name">
-            {name}
-        </span>
-        <span className="price">
-            {price}
-        </span>
+    return (
+        <div className="collection-item" key={id}>
+            <div
+                className="image"
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            />
 
-    </div>
-)
+            <span className="name">
+                {name}
+            </span>
+            <span className="price">
+                {price}
+            </span>
+
+        </div>
+    );
+}
 
 
 export default CollectionItems;
--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -2,19 +2,17 @@ import React from "react";
 import './collection-preview.styles.scss';
 import CollectionItems from "../collection-item/collection-item.component";
 
-
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {items
                 .filter((item, index) => index < 4)
-                .map((id, ...otherItemProps) =>
-                    < CollectionItems key={id} {...otherItemProps} />
-                )}
+                .map(item => (
+                    <CollectionItems key={item.id} {...item} />
+                ))}
         </div>
     </div>
-)
-
+);
 
 export default CollectionPreview;
